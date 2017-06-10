4619f7fdaa3a1bf8f6fa0c847cb4f945cd1881e5
mahdiarn <diarn_7@live.com>
2017-06-11 05:14:40 +0700

    Aquos
    homepage + bootstrap configuration

---
+++ aquos/index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<link href="css/full-slider.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<style>
+	.dropdown-submenu {
+		position: relative;
+	}
+	
+	.dropdown-submenu .dropdown-menu {
+		top: 0;
+		left: 100%;
+		margin-top: -1px;
+	}	
+	.navbar {
+		margin-bottom:0px;
+	}
+	</style>
+  </head>
+  <body>
+ <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">{INSERT LOGO HERE}</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+			<li role="separator" class="divider"></li>
+			<li class="dropdown-submenu">
+			<a class="test" tabindex="-1" href="#">Audit</a>
+			<ul class="dropdown-menu">		
+				<li role="separator" class="divider"></li>			
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Social</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">SMETA/Sedek Audit</a></li>
+						<li><a href="#">COC Audit</a></li>
+						<li><a href="#">Social Compliment Audit</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Quality System</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">Consultant Audit</a></li>
+						<li><a href="#">Pre Audit</a></li>
+						<li><a href="#">Gap Analysis Audit</a></li>				
+					</ul>
+				</li>
+				<li role="separator" class="divider"></li>
+			</ul>
+			</li>
+			<li class="dropdown-submenu">
+			<a class="test" tabindex="-1" href="#">Consultacy</a>
+			<ul class="dropdown-menu">
+			<li role="separator" class="divider"></li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Quality System</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">ISO 9001</a></li>
+						<li><a href="#">ISO/TS 16949</a></li>
+						<li><a href="#">ISO 28001</a></li>
+						<li><a href="#">ISO 31000</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Food Safety</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">ISO 22000</a></li>
+						<li><a href="#">HACCP</a></li>
+						<li><a href="#">FSSC 22000</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Environmental Management</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">ISO 14001</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Occupational Health and Safety</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">SMK3</a></li>
+						<li><a href="#">ISO 45001</a></li>
+						<li><a href="#">OHSAS 18001</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Social</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">FLA</a></li>
+						<li><a href="#">COC</a></li>
+						<li><a href="#">BSCI</a></li>
+						<li><a href="#">WRAP</a></li>
+						<li><a href="#">CTPAT</a></li>
+						<li><a href="#">SA8000</a></li>
+						<li><a href="#">SMETA/Sedek</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Marine System</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">ISM CODE</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li><a tabindex="-1" href="#">Quality Improvement & Productivity</a></li>
+				<li><a tabindex="-1" href="#">Quality Control Circle</a></li>
+			</ul>
+			</li>
+			<li class="dropdown-submenu">
+			<a class="test" tabindex="-1" href="#">Training</a>
+			<ul class="dropdown-menu">
+			<li role="separator" class="divider"></li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Quality System</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">ISO 9001</a></li>
+						<li><a href="#">ISO/TS 16949</a></li>
+						<li><a href="#">ISO 28001</a></li>
+						<li><a href="#">ISO 31000</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Food Safety</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">ISO 22000</a></li>
+						<li><a href="#">HACCP</a></li>
+						<li><a href="#">FSSC 22000</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Environmental Management</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">ISO 14001</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Occupational Health and Safety</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">SMK3</a></li>
+						<li><a href="#">ISO 45001</a></li>
+						<li><a href="#">OHSAS 18001</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Social</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">FLA</a></li>
+						<li><a href="#">COC</a></li>
+						<li><a href="#">BSCI</a></li>
+						<li><a href="#">WRAP</a></li>
+						<li><a href="#">CTPAT</a></li>
+						<li><a href="#">SA8000</a></li>
+						<li><a href="#">SMETA/Sedek</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li class="dropdown-submenu">
+				<a class="test" href="#">Marine System</span></a>
+					<ul class="dropdown-menu">
+						<li role="separator" class="divider"></li>
+						<li><a href="#">ISM CODE</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				<li><a tabindex="-1" href="#">Quality Improvement & Productivity</a></li>
+				<li><a tabindex="-1" href="#">Quality Control Circle</a></li>
+			</ul>
+			</li>			
+			<li role="separator" class="divider"></li>
+          </ul>
+        </li>
+		<li><a href="#">Articles <span class="sr-only"></span></a></li>
+		<li><a href="#">Contact Us <span class="sr-only"></span></a></li>
+      </ul>      
+	  <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-envelope"></span> aquos.consultant@yahoo.co.id</a></li>
+		<li><a href="#"><span class="glyphicon glyphicon-phone"></span> +628129257843</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+	<!-- Full Page Image Background Carousel Header -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <!-- Set the first background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('./img/c-1.jpeg');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 1</h2>
+					<p>Lorem ipsum dolor sit amet, per iuvaret delicata eu, cu eos tale tritani verterem. Vix euismod scribentur et, at vim tation timeam epicuri. Ne facer denique abhorreant nam. Ad eam quaeque fabellas oportere, eu pro cetero deterruisset, ex qui modo cetero propriae. Per ex legere vivendo reprehendunt.
+</p>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the second background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('./img/c-2.jpeg');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 2</h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the third background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('./img/c-3.jpeg');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 3</h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+
+    </header>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script>
+	$(document).ready(function(){
+		$('.dropdown-submenu a.test').on("click", function(e){
+			$(this).next('ul').toggle();
+			e.stopPropagation();
+			e.preventDefault();
+		});
+	});
+	</script>
+
+  </body>
+</html>
+
